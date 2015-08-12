e9ae6401d45c27c6a79d73c74de0d959ed166014
Rafael <rafaell.rochaa@gmail.com>
2015-08-12 18:18:41 -0300

    Primeiro commit criação de Areas

---
+++ Mabruk/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Mabruck.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "Mabruck.Areas.Admin.Controllers" }  //Para não ter conflito com os controllers de mesmo nome
+            );
+        }
+    }
+}
